47ad5f4cf49eb990993b95e5deb5aac4895a8fa2
subdavis <bdavis@redspin.net>
2017-01-31 23:23:31 -0500

    Include more bootstrap

---
+++ js/index.js
@@ -0,0 +1,45 @@
+var sortby = function(a, b) {return b[1] - a[1]};
+
+function tableCreate(el, data)
+{
+    var tbl  = document.createElement("table");
+    
+
+    for (var i = 0; i < data.length; ++i)
+    {
+        var tr = tbl.insertRow();
+        for(var j = 0; j < data[i].length; ++j)
+        {
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(data[i][j].toString()));
+        }
+    }
+    el.appendChild(tbl);
+}
+
+function update() {
+    $.getJSON('../meme/', function(data) {
+        
+    document.getElementById("jsonP").innerHTML = JSON.stringify(data, undefined, 2)
+    });
+
+
+    $.getJSON('../meme/stocks', function(data) {
+        var rows = Object.keys(data).map(function(key) {return  [key, data[key]]});
+
+    rows.sort(sortby);
+    tableCreate(document.getElementById("market"), rows);
+    });
+}
+update();
+
+function sell() {
+    var meme = document.getElementById("meme").value;
+    $.get("../meme/sell", {meme: meme}, update) 
+}
+
+
+function buy() {
+    var meme = document.getElementById("meme").value;
+    $.get("../meme/buy", {meme: meme}, update) 
+}
