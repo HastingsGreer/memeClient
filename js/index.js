ce6801930513fdfd6b10f6904551e66f4f420f36
Austin Wang <austin.wang0311@gmail.com>
2017-02-02 15:51:06 -0500

    oops

--- js/index.js
+++ js/index.js
@@ -63,7 +63,7 @@ function update() {
 
       var portfolioText = JSON.stringify(data, undefined, 2);
       portfolioText = portfolioText.substring(2, portfolioText.length-1).replace(/{(.*)/, '').replace(/(.*)}/);
-      portfolioText = portfolioText.replace(/\n(.*): 0,/gi,'');
+      portfolioText = portfolioText.replace(/\n(.*) 0,/gi,'');
       portfolioText = portfolioText.replace
       document.getElementById("jsonP").innerHTML = portfolioText;
       updateMarket();
