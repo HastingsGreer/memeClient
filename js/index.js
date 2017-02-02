630b3bc776e0ce8f0a3d932537c0c95cbac5b791
Austin Wang <austin.wang0311@gmail.com>
2017-02-02 15:10:02 -0500

    retouchup

--- js/index.js
+++ js/index.js
@@ -63,7 +63,7 @@ function update() {
 
       var portfolioText = JSON.stringify(data, undefined, 2);
       portfolioText = portfolioText.substring(2, portfolioText.length-1).replace(/{(.*)}/, '');
-      portfolioText = portfolioText.replace(/\n(.*)0,/,'');
+      portfolioText = portfolioText.replace(/(.*)0,/,'');
       document.getElementById("jsonP").innerHTML = portfolioText;
       updateMarket();
    });
